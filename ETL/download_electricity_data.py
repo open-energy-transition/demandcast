d9e634cc1c4e428625a0116c13f7d5eb8f13be02
Enrico Antonini <50218270+eantonini@users.noreply.github.com>
2025-08-01 09:15:47 +0000

    Renamed folders and started changing structure

--- ETL/download_electricity_data.py
+++ ETL/download_electricity_data.py
@@ -14,12 +14,11 @@ Description:
 """
 
 import argparse
-import importlib
 import logging
 import os
 from datetime import datetime
 
-import pandas
+import retrievals.electricity_demand
 import utils.directories
 import utils.entities
 import utils.time_series
@@ -146,250 +145,6 @@ def read_command_line_arguments() -> argparse.Namespace:
     return args
 
 
-def retrieve_data(data_source: str, code: str) -> pandas.Series:
-    """
-    Retrieve the electricity demand data.
-
-    This function retrieves the electricity demand time series from the
-    specified data source.
-
-    Parameters
-    ----------
-    data_source : str
-        The data source.
-    code : str
-        The code of the country or subdivision.
-
-    Returns
-    -------
-    electricity_demand_time_series : pandas.Series
-        The electricity demand time series in MW.
-    """
-    # Check if there is only one code in the data source.
-    one_code_in_data_source = (
-        len(utils.entities.read_codes(data_source=args.data_source)) == 1
-    )
-
-    # Import the retrieval module for the data source.
-    retrieval_module = importlib.import_module(
-        f"electricity_demand.{data_source}"
-    )
-
-    # Get the list of requests to retrieve the electricity demand time
-    # series.
-    if one_code_in_data_source:
-        # If there is only one code in the data source, there is no need
-        # to specify the code.
-        requests = retrieval_module.get_available_requests()
-    else:
-        # If there are multiple codes in the data source, the code needs
-        # to be specified.
-        requests = retrieval_module.get_available_requests(code)
-
-    if requests is None:
-        # If there are no requests (requests is None), it means that the
-        # electricity demand time series can be retrieved all at once.
-        # Get the retrieval function to download and extract the data.
-        retrieval_function = retrieval_module.download_and_extract_data
-
-        if one_code_in_data_source:
-            # If there is only one code in the data source, there is no
-            # need to specify the code.
-            electricity_demand_time_series = retrieval_function()
-        else:
-            # If there are multiple codes in the data source, the code
-            # needs to be specified.
-            electricity_demand_time_series = retrieval_function(code)
-
-    else:
-        # If there are multiple requests (request is not None), loop
-        # over the requests to retrieve the electricity demand time
-        # series of each request.
-        # Get the retrieval function to download and extract the data.
-        retrieval_function = (
-            retrieval_module.download_and_extract_data_for_request
-        )
-
-        if one_code_in_data_source:
-            # If there is only one code in the data source, there is no
-            # need to specify the code.
-            # Loop over the requests to retrieve the electricity demand
-            # time series of each request.
-            electricity_demand_time_series_list = [
-                retrieval_function(*request)
-                if isinstance(request, tuple)
-                else retrieval_function(request)
-                for request in requests
-            ]
-        else:
-            # If there are multiple codes in the data source, the code
-            # needs to be specified.
-            # Loop over the requests to retrieve the electricity demand
-            # time series of each request.
-            electricity_demand_time_series_list = [
-                retrieval_function(*request, code)
-                if isinstance(request, tuple)
-                else retrieval_function(request, code)
-                for request in requests
-            ]
-
-        # Remove empty time series.
-        electricity_demand_time_series_list = [
-            time_series
-            for time_series in electricity_demand_time_series_list
-            if not time_series.empty
-        ]
-
-        # Concatenate the electricity demand time series of all periods.
-        electricity_demand_time_series = pandas.concat(
-            electricity_demand_time_series_list
-        )
-
-    # Clean the data.
-    electricity_demand_time_series = utils.time_series.clean_data(
-        electricity_demand_time_series, "Load (MW)"
-    )
-
-    return electricity_demand_time_series
-
-
-def save_data(
-    electricity_demand_time_series: pandas.Series,
-    data_source: str,
-    code: str,
-    upload_to_gcs: str | None,
-    upload_to_zenodo: bool,
-    publish_to_zenodo: bool,
-    made_by_oet: bool,
-) -> None:
-    """
-    Save the electricity demand time series.
-
-    This function saves the electricity demand time series to a file in
-    the parquet and csv formats. If a Google Cloud Storage (GCS) bucket
-    name is provided, it uploads the parquet file to the specified GCS
-    bucket.
-
-    Parameters
-    ----------
-    electricity_demand_time_series : pandas.Series
-        The electricity demand time series in MW.
-    data_source : str
-        The data source.
-    code : str
-        The code of the country or subdivision.
-    upload_to_gcs : str
-        The bucket name of the Google Cloud Storage (GCS) to upload the
-        data.
-    upload_to_zenodo : bool
-        Whether to upload the data to Zenodo.
-    publish_to_zenodo : bool
-        Whether to publish the data to Zenodo.
-    made_by_oet : bool
-        Whether the data was made by Open Energy Transition.
-    """
-    # Get the date of retrieval.
-    date_of_retrieval = pandas.Timestamp.today().strftime("%Y-%m-%d")
-
-    # Get the directory to store the electricity demand time series.
-    result_directory = utils.directories.read_folders_structure()[
-        "electricity_demand_folder"
-    ]
-    result_directory = os.path.join(result_directory, date_of_retrieval)
-    os.makedirs(result_directory, exist_ok=True)
-
-    # Define the identifier of the file to be saved.
-    identifier = code + "_" + data_source
-
-    # Define the path to the file to be saved without the extension.
-    file_path = os.path.join(result_directory, identifier)
-
-    # Save the time series to both parquet and csv files.
-    electricity_demand_time_series.to_frame().to_parquet(
-        file_path + ".parquet"
-    )
-    electricity_demand_time_series.to_csv(file_path + ".csv")
-
-    if upload_to_gcs:
-        # Upload the parquet file of the electricity demand time series
-        # to GCS.
-        utils.uploader.upload_to_gcs(
-            file_path + ".parquet",
-            upload_to_gcs,
-            "upload_" + date_of_retrieval + "/" + identifier + ".parquet",
-        )
-
-    # Import the retrieval module for the data source.
-    retrieval_module = importlib.import_module(
-        f"electricity_demand.{data_source}"
-    )
-
-    if upload_to_zenodo and retrieval_module.redistribute():
-        # Upload the parquet file of the electricity demand time series
-        # to Zenodo.
-        utils.uploader.upload_to_zenodo(
-            file_path + ".parquet",
-            data_type="actual",
-            made_by_oet=made_by_oet,
-            publish=publish_to_zenodo,
-            testing=True,
-        )
-    elif upload_to_zenodo and not retrieval_module.redistribute():
-        logging.warning(
-            f"The data source {data_source} does not support redistribution "
-            "to Zenodo. The data will not be uploaded to Zenodo."
-        )
-
-
-def run_data_retrieval(args: argparse.Namespace) -> None:
-    """
-    Run the electricity demand data retrieval.
-
-    This function runs the electricity demand data retrieval
-    process. It retrieves the electricity demand time series for the
-    specified countries and subdivisions from the data source, cleans
-    the data, and saves it.
-
-    Parameters
-    ----------
-    args : argparse.Namespace
-        The command line arguments.
-    """
-    # Get the list of codes of the countries and subdivisions of
-    # interest.
-    codes = utils.entities.check_and_get_codes(
-        args.code, args.data_source, args.file
-    )
-
-    logging.info(
-        f"Retrieving electricity data from the {args.data_source} website."
-    )
-
-    # Loop over the codes.
-    for code in codes:
-        logging.info(f"Retrieving data for {code}.")
-
-        # Retrieve the electricity demand time series.
-        electricity_demand_time_series = retrieve_data(args.data_source, code)
-
-        # Save the electricity demand time series to a file and upload
-        # it to GCS.
-        save_data(
-            electricity_demand_time_series,
-            args.data_source,
-            code,
-            args.upload_to_gcs,
-            args.upload_to_zenodo,
-            args.publish_to_zenodo,
-            args.made_by_oet,
-        )
-
-    logging.info(
-        f"Electricity data from the {args.data_source} website has been "
-        "successfully retrieved and saved."
-    )
-
-
 if __name__ == "__main__":
     # Read the command line arguments.
     args = read_command_line_arguments()
@@ -412,4 +167,12 @@ if __name__ == "__main__":
     )
 
     # Run the data retrieval.
-    run_data_retrieval(args)
+    retrievals.electricity_demand.run_data_retrieval(
+        args.code,
+        args.data_source,
+        args.file,
+        args.upload_to_gcs,
+        args.upload_to_zenodo,
+        args.publish_to_zenodo,
+        args.made_by_oet,
+    )
---
+++ ETL/retrievals/electricity_demand.py
@@ -0,0 +1,287 @@
+# -*- coding: utf-8 -*-
+"""
+License: AGPL-3.0.
+
+Description:
+
+    This module handles the retrieval of electricity demand data from
+    various sources. It retrieves the electricity demand time series
+    for specified countries and subdivisions, cleans the data, and saves
+    it in a structured format. The module supports uploading the data to
+    Google Cloud Storage (GCS) and Zenodo if specified.
+"""
+
+import importlib
+import logging
+import os
+
+import pandas
+import utils.directories
+import utils.entities
+import utils.time_series
+import utils.uploader
+
+
+def retrieve_data(data_source: str, code: str) -> pandas.Series:
+    """
+    Retrieve the electricity demand data.
+
+    This function retrieves the electricity demand time series from the
+    specified data source.
+
+    Parameters
+    ----------
+    data_source : str
+        The data source.
+    code : str
+        The code of the country or subdivision.
+
+    Returns
+    -------
+    electricity_demand_time_series : pandas.Series
+        The electricity demand time series in MW.
+    """
+    # Check if there is only one code in the data source.
+    one_code_in_data_source = (
+        len(utils.entities.read_codes(data_source=data_source)) == 1
+    )
+
+    # Import the retrieval module for the data source.
+    retrieval_module = importlib.import_module(
+        f"electricity_demand_data_sources.{data_source}"
+    )
+
+    # Get the list of requests to retrieve the electricity demand time
+    # series.
+    if one_code_in_data_source:
+        # If there is only one code in the data source, there is no need
+        # to specify the code.
+        requests = retrieval_module.get_available_requests()
+    else:
+        # If there are multiple codes in the data source, the code needs
+        # to be specified.
+        requests = retrieval_module.get_available_requests(code)
+
+    if requests is None:
+        # If there are no requests (requests is None), it means that the
+        # electricity demand time series can be retrieved all at once.
+        # Get the retrieval function to download and extract the data.
+        retrieval_function = retrieval_module.download_and_extract_data
+
+        if one_code_in_data_source:
+            # If there is only one code in the data source, there is no
+            # need to specify the code.
+            electricity_demand_time_series = retrieval_function()
+        else:
+            # If there are multiple codes in the data source, the code
+            # needs to be specified.
+            electricity_demand_time_series = retrieval_function(code)
+
+    else:
+        # If there are multiple requests (request is not None), loop
+        # over the requests to retrieve the electricity demand time
+        # series of each request.
+        # Get the retrieval function to download and extract the data.
+        retrieval_function = (
+            retrieval_module.download_and_extract_data_for_request
+        )
+
+        if one_code_in_data_source:
+            # If there is only one code in the data source, there is no
+            # need to specify the code.
+            # Loop over the requests to retrieve the electricity demand
+            # time series of each request.
+            electricity_demand_time_series_list = [
+                retrieval_function(*request)
+                if isinstance(request, tuple)
+                else retrieval_function(request)
+                for request in requests
+            ]
+        else:
+            # If there are multiple codes in the data source, the code
+            # needs to be specified.
+            # Loop over the requests to retrieve the electricity demand
+            # time series of each request.
+            electricity_demand_time_series_list = [
+                retrieval_function(*request, code)
+                if isinstance(request, tuple)
+                else retrieval_function(request, code)
+                for request in requests
+            ]
+
+        # Remove empty time series.
+        electricity_demand_time_series_list = [
+            time_series
+            for time_series in electricity_demand_time_series_list
+            if not time_series.empty
+        ]
+
+        # Concatenate the electricity demand time series of all periods.
+        electricity_demand_time_series = pandas.concat(
+            electricity_demand_time_series_list
+        )
+
+    # Clean the data.
+    electricity_demand_time_series = utils.time_series.clean_data(
+        electricity_demand_time_series, "Load (MW)"
+    )
+
+    return electricity_demand_time_series
+
+
+def save_data(
+    electricity_demand_time_series: pandas.Series,
+    code: str,
+    data_source: str,
+    upload_to_gcs: str | None,
+    upload_to_zenodo: bool,
+    publish_to_zenodo: bool,
+    made_by_oet: bool,
+) -> None:
+    """
+    Save the electricity demand time series.
+
+    This function saves the electricity demand time series to a file in
+    the parquet and csv formats. If a Google Cloud Storage (GCS) bucket
+    name is provided, it uploads the parquet file to the specified GCS
+    bucket.
+
+    Parameters
+    ----------
+    electricity_demand_time_series : pandas.Series
+        The electricity demand time series in MW.
+    data_source : str
+        The data source.
+    code : str
+        The code of the country or subdivision.
+    upload_to_gcs : str
+        The bucket name of the Google Cloud Storage (GCS) to upload the
+        data.
+    upload_to_zenodo : bool
+        Whether to upload the data to Zenodo.
+    publish_to_zenodo : bool
+        Whether to publish the data to Zenodo.
+    made_by_oet : bool
+        Whether the data was made by Open Energy Transition.
+    """
+    # Get the date of retrieval.
+    date_of_retrieval = pandas.Timestamp.today().strftime("%Y-%m-%d")
+
+    # Get the directory to store the electricity demand time series.
+    result_directory = utils.directories.read_folders_structure()[
+        "electricity_demand_folder"
+    ]
+    result_directory = os.path.join(result_directory, date_of_retrieval)
+    os.makedirs(result_directory, exist_ok=True)
+
+    # Define the identifier of the file to be saved.
+    identifier = code + "_" + data_source
+
+    # Define the path to the file to be saved without the extension.
+    file_path = os.path.join(result_directory, identifier)
+
+    # Save the time series to both parquet and csv files.
+    electricity_demand_time_series.to_frame().to_parquet(
+        file_path + ".parquet"
+    )
+    electricity_demand_time_series.to_csv(file_path + ".csv")
+
+    if upload_to_gcs:
+        # Upload the parquet file of the electricity demand time series
+        # to GCS.
+        utils.uploader.upload_to_gcs(
+            file_path + ".parquet",
+            upload_to_gcs,
+            "upload_" + date_of_retrieval + "/" + identifier + ".parquet",
+        )
+
+    # Import the retrieval module for the data source.
+    retrieval_module = importlib.import_module(
+        f"electricity_demand_data_sources.{data_source}"
+    )
+
+    if upload_to_zenodo and retrieval_module.redistribute():
+        # Upload the parquet file of the electricity demand time series
+        # to Zenodo.
+        utils.uploader.upload_to_zenodo(
+            file_path + ".parquet",
+            data_type="actual",
+            made_by_oet=made_by_oet,
+            publish=publish_to_zenodo,
+            testing=True,
+        )
+    elif upload_to_zenodo and not retrieval_module.redistribute():
+        logging.warning(
+            f"The data source {data_source} does not support redistribution "
+            "to Zenodo. The data will not be uploaded to Zenodo."
+        )
+
+
+def run_data_retrieval(
+    code: str,
+    data_source: str,
+    file: str,
+    upload_to_gcs: str | None,
+    upload_to_zenodo: bool,
+    publish_to_zenodo: bool,
+    made_by_oet: bool,
+) -> None:
+    """
+    Run the electricity demand data retrieval.
+
+    This function runs the electricity demand data retrieval
+    process. It retrieves the electricity demand time series for the
+    specified countries and subdivisions from the data source, cleans
+    the data, and saves it.
+
+    Parameters
+    ----------
+    code : str
+        The code of the country or subdivision.
+    data_source : str
+        The data source from which to retrieve the electricity demand
+        time series.
+    file : str
+        The path to the yaml file containing the list of codes of the
+        countries and subdivisions of interest.
+    upload_to_gcs : str | None
+        The bucket name of the Google Cloud Storage (GCS) to upload the
+        data.
+    upload_to_zenodo : bool
+        Whether to upload the data to Zenodo.
+    publish_to_zenodo : bool
+        Whether to publish the data to Zenodo.
+    made_by_oet : bool
+        Whether the data was made by Open Energy Transition.
+    """
+    # Get the list of codes of the countries and subdivisions of
+    # interest.
+    codes = utils.entities.check_and_get_codes(code, data_source, file)
+
+    logging.info(
+        f"Retrieving electricity data from the {data_source} website."
+    )
+
+    # Loop over the codes.
+    for code in codes:
+        logging.info(f"Retrieving data for {code}.")
+
+        # Retrieve the electricity demand time series.
+        electricity_demand_time_series = retrieve_data(data_source, code)
+
+        # Save the electricity demand time series to a file and upload
+        # it to GCS.
+        save_data(
+            electricity_demand_time_series,
+            code,
+            data_source,
+            upload_to_gcs,
+            upload_to_zenodo,
+            publish_to_zenodo,
+            made_by_oet,
+        )
+
+    logging.info(
+        f"Electricity data from the {data_source} website has been "
+        "successfully retrieved and saved."
+    )
