dee037804193ac52b22440331a8164847d6142ef
Enrico Antonini <50218270+eantonini@users.noreply.github.com>
2025-08-06 09:20:26 +0000

    Deleted original scripts

--- ETL/retrieve.py
+++ ETL/retrieve.py
@@ -232,7 +232,8 @@ if __name__ == "__main__":
             args.made_by_oet,
         )
     elif args.variable == "annual_electricity_demand_per_capita":
-        # Run the data retrieval for annual electricity demand per capita.
+        # Run the data retrieval for annual electricity demand per
+        # capita.
         retrievals.annual_electricity_demand_per_capita.run_data_retrieval(
             args.code,
             args.file,
