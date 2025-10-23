713b81b9343a4f3de3566f7f8767e3ebc1de880d
Vamsipriya22 <golivamsipriya22@gmail.com>
2025-10-23 10:57:03 +0530

    Modified grupoice.py

--- ETL/retrievals/grupoice.py
+++ ETL/retrievals/grupoice.py
@@ -7,11 +7,7 @@ Description:
     This module provides functions to retrieve the electricity demand
     data from the website of the Grupo ICE (GRUPOICE) in Costa Rica.
     The data is downloaded from Mar 01, 2012 up to the current date.
-    The data is retrieved in one-day intervals.
-
-    Note:
-    Retrieving all the data takes a few hours due to the large
-    number of CSV files that need to be processed.
+    The data is retrieved in one-year intervals.
 
     Source: https://apps.grupoice.com/CenceWeb
 """
@@ -32,31 +28,29 @@ def redistribute() -> bool:
     bool
         True if the data can be redistributed, False otherwise.
     """
-    logging.debug("All rights reserved by GRUPOICE.")
-    logging.debug("Source: https://apps.grupoice.com/CenceWeb")
-    return False
+    logging.debug("Open data.")
+    logging.debug(
+        "Source: https://www.grupoice.com/wps/wcm/connect/328d1cc7-6796-44cb-a981-8dca6043c983/Reglamento_funcionamiento_CENCE.pdf?MOD=AJPERES&CACHEID=ROOTWORKSPACE-328d1cc7-6796-44cb-a981-8dca6043c983-nWcNMD."  # noqa: W505
+    )
+    return True
 
 
-def _check_input_parameters(year: int, month: int, day: int) -> None:
+def _check_input_parameters(year: int) -> None:
     """
     Check if the input parameters are valid.
 
     Parameters
     ----------
     year : int
-        The year of the data.
-    month : int
-        The month of the data.
-    day : int
-        The day of the data.
+        The year of the data to retrieve.
     """
-    # Check if the input parameters are valid.
-    assert (year, month, day) in get_available_requests(), (
-        f"The {year}-{month:02d}-{day:02d} request is not available."
+    # Check if the year is supported.
+    assert year in get_available_requests(), (
+        f"The year {year} is not in the supported range."
     )
 
 
-def get_available_requests() -> list[tuple[int, int, int]]:
+def get_available_requests() -> list[int]:
     """
     Get the available requests.
 
@@ -65,70 +59,53 @@ def get_available_requests() -> list[tuple[int, int, int]]:
 
     Returns
     -------
-    requests : list[tuple[int, int, int]]
-        List of tuples in the format (year, month, day).
+    list[int]
+        The list of available requests.
     """
-    # Get the start and end dates for Singapore.
+    # Read the start and end date of the available data.
     start_date, end_date = utils.entities.read_date_ranges(
         data_source="grupoice"
     )["CR"]
 
-    # Subtract 5 days from the end date to ensure that the last
-    # request is within the available data range.
-    end_date = end_date - pandas.Timedelta("5days")
-
-    # Return the available requests, which are tuples in the format
-    # (year, month, day).
-    return [
-        (date.year, date.month, date.day)
-        for date in pandas.date_range(
-            start=start_date, end=end_date, freq="1D"
-        )
-    ]
+    # Return the available requests, which are the years.
+    return list(range(start_date.year, end_date.year + 1))
 
 
-def get_url(year: int, month: int, day: int) -> str:
+def get_url(year: int) -> str:
     """
     Get the URL of the electricity demand data on the GRUPOICE website.
 
     Parameters
     ----------
     year : int
-        The year of the data.
-    month : int
-        The month of the data.
-    day : int
-        The day of the data.
+        The year of the electricity demand data.
 
     Returns
     -------
     str
         The URL of the electricity demand data.
     """
-    # Check if the input parameters are valid.
-    _check_input_parameters(year, month, day)
+    # Check if input parameters are valid.
+    _check_input_parameters(year)
 
     # Construct the URL for the request.
     return (
         "https://apps.grupoice.com/CenceWeb/data/sen/csv/DemandaMW?intervalo=15&"
-        f"inicio={year}{month:02d}{day:02d}&fin={year}{month:02d}{day:02d}"
+        f"inicio={year}0101&fin={year}1231"
     )
 
 
-def download_and_extract_data_for_request(
-    year: int, month: int, day: int
-) -> pandas.Series:
+def download_and_extract_data_for_request(year: int) -> pandas.Series:
     """
-    Download and extract electricity demand data from GRUPOICE website.
+    Download and extract electricity demand data.
+
+    This function downloads and extracts the electricity demand data
+    from the GRUPOICE website.
 
     Parameters
     ----------
     year : int
-        The year of the data.
-    month : int
-        The month of the data.
-    day : int
-        The day of the data.
+        The year of the electricity demand data.
 
     Returns
     -------
@@ -140,15 +117,13 @@ def download_and_extract_data_for_request(
     ValueError
         If the extracted data is not a pandas DataFrame.
     """
-    # Check if the input parameters are valid.
-    _check_input_parameters(year, month, day)
+    # Check if input parameters are valid.
+    _check_input_parameters(year)
 
-    logging.info(
-        f"Retrieving electricity demand data for {year}-{month:02d}-{day:02d}."
-    )
+    logging.info(f"Retrieving electricity demand data for the year {year}.")
 
-    # Construct the URL for the request.
-    url = get_url(year, month, day)
+    # Get the URL of the electricity demand data.
+    url = get_url(year)
 
     # Fetch the data from the URL.
     dataset = utils.fetcher.fetch_data(
