f442c6b30654be2752b7b4ad361903d7ed889570
Enrico Antonini <50218270+eantonini@users.noreply.github.com>
2025-11-17 09:46:10 +0000

    Added init file in models folder

---
+++ demandcast/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+License: AGPL-3.0.
+
+Description:
+
+    This package contains modules for training machine learning models
+    and making forecasts of electricity demand.
+"""
