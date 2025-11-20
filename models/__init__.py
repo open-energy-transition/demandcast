1c647b233c555af0ce1d4a77028e4c532db46f0f
Enrico Antonini <50218270+eantonini@users.noreply.github.com>
2025-11-20 11:19:25 +0000

    Deleted init file

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-License: AGPL-3.0.
-
-Description:
-
-    This package contains modules for training machine learning models
-    and making forecasts of electricity demand.
-"""
